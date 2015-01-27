a163c12aecb186d2ad963a31288ec72c09569be9
harshalic <harshalichopade@gmail.com>
2015-01-27 11:38:01 +0530

    new file:   front/cURLRequest.php 	new file:   front/hello.cgi 	new file:   front/index.php 	new file:   front/modify/index.php 	new file:   front/symfonyClient/cURL/cURLRequest.php 	new file:   front/symfonyClient/index.php

---
+++ front/cURLRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+
+class cURL{
+
+	
+	
+	public function cURL_GET($path){
+                $baseUrl = "http://localhost:8000";
+		$requestUrl = $baseUrl.$path;
+		
+		if(is_callable('curl_init')){
+		//echo 'calling-> '. $requestUrl; 
+                $ch = curl_init($requestUrl);
+		
+		//set the url, number of POST vars, POST data
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    	
+		$res = curl_exec($ch);
+		curl_close($ch);
+		return $res;
+		}else {
+			echo 'cURL not enabled';
+		}
+	}
+
+	
+	public function cURL_POST($path,$sendData){
+		
+		$baseUrl = "http://localhost:8000";
+		$requestUrl = $baseUrl.$path;
+		$fields_string='';
+		
+		if(is_callable('curl_init')){
+		//echo 'calling-> '. $requestUrl; 
+                $ch = curl_init($requestUrl);
+		
+		//url-ify the data for the POST
+		foreach($sendData as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+		rtrim($fields_string, '&');
+		
+		//set the url, number of POST vars, POST data
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch,CURLOPT_POST, count($_POST));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+    	
+    	
+		$res = curl_exec($ch);
+		curl_close($ch);
+		return $res;
+		}else {
+			echo 'cURL not enabled';
+		}
+	}
+	
+	
+}
+
+
+
+
+?>
